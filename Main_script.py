284dcba587d6ed04945ee8abd5d0e07df32c2aed
carllman13 <78727496+carllman13@users.noreply.github.com>
2023-01-01 01:44:17 +0000

    Final Version
    Run codes in Main_scripts; functions are defined in FedCurve; set username/pw in User.

---
+++ Main_script.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import FedCurve
+import time
+
+
+# In[ ]:
+
+
+#Set meetings
+#need at least 2 meetings, in chronological order, of format 'FED-YYMMM' e.g.meetings = ['FED-23SEP','FED-23DEC']
+#we have default_meetings = most recently settled contract + all unclosed meetings
+meetings = FedCurve.default_meetings() 
+#meetings = ['FED-23SEP','FED-23NOV']
+
+#Set lookback, default lookback =90
+lookback = FedCurve.lookback
+#lookback = 10
+
+
+# In[ ]:
+
+
+#Get historical implied rate df - takes time
+c = time.time()
+meetings_implied_df = FedCurve.get_meetings_implied_df(series_ids = meetings,lookback = lookback)
+d = time.time()
+print(f'Process took {round((d-c)/60,2)}min')
+
+
+# In[ ]:
+
+
+#Plotting dashboard
+FedCurve.plot_historical_curve(meetings_implied_df)
+
+
+# ## Checking specific data points
+
+# e.g. We see an outlier on 23MAR Meeting on 22-10-21 from our dashboard, so we dig into the details by calling the check_meeting function:
+
+# In[ ]:
+
+
+historical_prices,contracts = FedCurve.check_meeting('23MAR',lookback = lookback)
+
+
+# We then look at all strike contracts on the outlier date:
+
+# In[ ]:
+
+
+FedCurve.check_meeting_ondate(historical_prices,2022,10,21)
+
+
+# - We see it's the '>3.25%' contract that's causing the anomolies (prices of lower strikes should >= higher strikes, since our CDF should be a non-decreasing step function)
+# - So we pull up the detailed history for this specific strike over time
+
+# In[ ]:
+
+
+FedCurve.check_raw_data(contracts, '3.25%',2022,10,21)
+
+
+# We see that the price drop is due to sudden increase in spread,(bid/ask widened to 94) hence we couldn't have profited from this. 
---
+++ User.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+import pandas as pd
+from datetime import datetime
+
+
+# ## Username and Password
+
+# In[1]:
+
+
+username = ''
+password = ''
+
+
+# ## Other parameters
+
+# In[ ]:
+
+
+#Manually adjust historical FOMCs
+#Date is set to be actual release date + 1
+#e.g. 22Dec meeting took place on Dec 14th, 2022, so We set the confirmed rate to be updated on Dec 15th, 2022.
+actual_FOMC={
+    '22SEP':[datetime(2022,9,22),300],
+    '22NOV':[datetime(2022,11,3),375],
+    '22DEC': [datetime(2022,12,15),425],
+    }
+actual_FOMC_df = pd.DataFrame.from_dict(actual_FOMC,orient= 'index',columns = ['Update Date','FFR in bps'])
+
+#default historical window is 90
+lookback = 90
+
